4dcba45be296e38ca00bd44ba3f58ca7839d756c
bobtas <bogdan.tasciuc@gmail.com>
2020-07-02 13:17:59 +0300

    salonfaq

---
+++ cautsalonapp/Controllers/despre_noiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cautsalonapp.Controllers
+{
+    public class despre_noiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
