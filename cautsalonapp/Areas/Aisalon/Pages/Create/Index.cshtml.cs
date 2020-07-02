bb6ef2c7c8930c57b3e75854f406b925a0747f9c
bobtas <bogdan.tasciuc@gmail.com>
2020-07-02 16:12:11 +0300

    More fixes. tralalalala

---
+++ cautsalonapp/Areas/Aisalon/Pages/Create/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cautsalonapp.Areas.Aisalon.Pages.Create
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
